1423fffec8f99719813b825afddc196d06c001e8
Bhuang5 <Bhuang5@stuy.edu>
2016-11-09 21:46:48 -0500

    Add files via upload

---
+++ signal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
+
+
+static void sighandler (int signo){
+  if (signo == SIGINT){
+    printf("Signal caught: %d\n", signo);
+    int fd = open("file.txt", O_CREAT | O_WRONLY | O_APPEND, 0644);
+    write(fd,"\nFile exiting: SIGINT\n",25);
+    close(fd);
+    exit(0);
+  }
+
+  else if (signo == SIGUSR1)
+    printf("PID of parent process: %d \n", getppid());
+}
+
+int main(){
+  signal(SIGINT,sighandler);
+  signal(SIGUSR1,sighandler);
+while(1){
+    printf("Current PID: %d\n",getpid());
+    sleep(1);
+  }
+
+  return 0;
+}
